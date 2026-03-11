85ae2e45bd363023300bf73612a57fa406a7ae0d
liuloulou <leo.adonia@gmail.com>
2026-03-11 14:54:19 +0800

    feat: add S3 plugin

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,16 +1,45 @@
-import type { Core } from '@strapi/strapi';
+import type { Core } from "@strapi/strapi";
+
+const S3_CDN = process.env.S3_CDN || "https://media.platoblog.com";
+const S3_BUCKET = process.env.S3_BUCKET || "platobox-cms-media";
+const S3_REGION = process.env.S3_REGION || "eu-central-1";
 
 const config: Core.Config.Middlewares = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            `${S3_BUCKET}.s3.${S3_REGION}.amazonaws.com`,
+            S3_CDN,
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            `${S3_BUCKET}.s3.${S3_REGION}.amazonaws.com`,
+            S3_CDN,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
 
 export default config;
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,33 @@
-import type { Core } from '@strapi/strapi';
+import type { Core } from "@strapi/strapi";
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({
+  env,
+}: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        baseUrl: env("S3_CDN", "https://media.platoblog.com"),
+        s3Options: {
+          credentials: {
+            accessKeyId: env("S3_ACCESS_KEY_ID"),
+            secretAccessKey: env("S3_ACCESS_SECRET"),
+          },
+          region: env("S3_REGION", "eu-central-1"),
+          params: {
+            ACL: env("S3_ACL", "public-read"),
+            signedUrlExpires: env("S3_SIGNED_URL_EXPIRES", 15 * 60),
+            Bucket: env("S3_BUCKET", "platobox-cms-media"),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
 
 export default config;
